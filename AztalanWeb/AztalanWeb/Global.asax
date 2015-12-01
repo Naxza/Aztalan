1a3ce069ed1d42a5a4d25eaa51b797955062a4d8
Josh Hoffmann <HoffmannJ28@uww.edu>
2015-12-01 15:33:53 -0600

    Web Layout
    Basic web layout. Currently in progress.

---
+++ AztalanWeb/AztalanWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AztalanWeb.MvcApplication" Language="C#" %>
---
+++ AztalanWeb/AztalanWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AztalanWeb.Startup))]
+namespace AztalanWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
